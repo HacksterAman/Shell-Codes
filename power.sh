fbc9d0a1dc718eb451a027c7627a1663d8713391
Aman Singh <aman952373@gmail.com>
2023-10-24 19:37:38 +0530

    modified:   power.sh

--- power.sh
+++ power.sh
@@ -1,17 +1,17 @@
 #!/bin/sh
 
-echo "Enter the your number: "
+echo Enter the your number:
 
 read n
 
-echo "Enter the power of your number: " 
+echo Enter the power of your number:
 
 read p
 
-while [ "$p" -gt 1 ]
+while [ $p -gt 1 ]
 do
 	n=$(($n * $n))
     p=$(($p - 1))
 done
 
-echo $n
+echo Result: $n
